65440a9fc3797bf5e2c44495b8ecd578bccb9538
Darryl Lee <darryllee1999@gmail.com>
2020-09-11 17:29:11 -0400

    Fixed and run the program on CS Servers

--- Assignment_0/simple_c_program.c
+++ Assignment_0/simple_c_program.c
@@ -4,9 +4,10 @@ Class: CS4541
 Assignment: Assignment 0 - Simple C Program  
 Author(s): Darryl Ming Sen Lee 
 */
+#include<stdio.h>
 
 int main(){
-    print("Hello World!\n");
+    printf("Hello World!\n");
 
     return 0;
 }
