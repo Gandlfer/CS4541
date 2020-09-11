b2412948f133a7da39a93c511c45327f0edd63c0
CawFee <darrylmingsen.lee@wmich.edu>
2020-09-11 16:57:16 -0400

    Initial Files setup

---
+++ README.txt
@@ -0,0 +1,6 @@
+Date: 09/11/2020  
+Class: CS4541  
+Assignment: Assignment 0 - Simple C Program  
+Author(s): Darryl Ming Sen Lee 
+
+Command to run the code: make flash
---
+++ simple_c_program.c
@@ -0,0 +1,12 @@
+/*
+Date: 09/11/2020  
+Class: CS4541  
+Assignment: Assignment 0 - Simple C Program  
+Author(s): Darryl Ming Sen Lee 
+*/
+
+int main(){
+    print("Hello World!\n");
+
+    return 0;
+}
