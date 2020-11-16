483a5899b92ac3f816dcefec171e532d8edda8b8
CawFee <darrylmingsen.lee@wmich.edu>
2020-11-16 06:29:51 -0500

    Updates Application Running instuctions from inputed text files

--- Assignment_3/Application.py
+++ Assignment_3/Application.py
@@ -1,16 +1,43 @@
 from FileReader import FileReader
-
+from Implicit import Implicit
+#from Explicit import Explicit
 import sys
 
 if __name__=="__main__":
     #if(len(sys.argv)>=1):
     if(1):
+        stack={}
+
         instruction=FileReader("Assignment_3\input.txt").openFileObject()#sys.argv[1]
         for x in instruction:
             print(x)
+        heapType=None
 
-        listType=input("1 for Implicit\n2 for Explicit\nEnter:")
-        fitType=input("1 for First Fit\n2 for Next Fit\nEnter:")
+        listType=input("0 for Implicit\n1 for Explicit\nEnter:")
+        print(listType)
+        fitType=input("0 for First Fit\n1 for Best Fit\nEnter:")
+        print("Called")
+        if(listType=="0"):
+            heapType=Implicit(int(fitType))
+        elif(listType=="1"):
+            #heapType=Explicit(fitType)
+            exit()
+
+        for x in instruction:
+            splitted=x.split(", ")
+            if(splitted[0]=='a'):
+                #print("Calling for "+splitted[1])
+                stack[splitted[2]]=heapType.myalloc(int(splitted[1]))
+            elif(splitted[0]=='f'):
+                heapType.myfree(stack[splitted[1]])
+                stack[splitted[1]]=-1
+                
+            elif(splitted[0]=='r'):
+                stack[splitted[3]]=heapType.myrealloc(stack[splitted[2]],int(splitted[1]))
+                stack[splitted[2]]=-1
+
+        heapType.printHeap()
+
     else:
         print("Invalid format!\n")
         print("Usage: python3 Application.py <textfile_path>\n\t")
