a3ce6cf3a3b3c29460bd8e7178e7a7a1c62fc6d3
CawFee <darrylmingsen.lee@wmich.edu>
2020-11-16 06:27:21 -0500

    Added new constant values Removed commented code Added padding data constant value Added data constant value

--- Assignment_3/HeapClass.py
+++ Assignment_3/HeapClass.py
@@ -1,16 +1,9 @@
 class Heap:
-    # wordByte=4
-    # alignment=8
-    # initialHeapsize=1000
-    # startHeapPointer=3
-    # # endHeapPointer=3
 
     def __init__(self):
-        #self.wordByte=4
-        #self.alignment=8
+        
         self.initialHeapsize=1000
-        #self.startHeapPointer=3
+        self.paddingData=255
+        self.data=170
         self.startHeapPointer=1
-        #self.endHeapPointer=3
-        #heap_array=[0]*self.wordByte*self.initialHeapsize
-        heap_array=[0]*self.initialHeapsize
+        self.heap_array=[0]*self.initialHeapsize
